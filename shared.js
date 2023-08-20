f79f6e42f917314a42ac24848c88d49e0d235377
Andrew <andrew_reynolds@mac.com>
2023-08-20 09:48:04 -0700

    added functionality to open and close side navigation on click

--- shared.js
+++ shared.js
@@ -1,12 +1,18 @@
 let backdrop = document.querySelector(".backdrop");
 let modal = document.querySelector(".modal");
+let modalNoButton = document.querySelector(".modal__action--negative");
 let selectPlanButton = document.querySelectorAll(".plan button");
-let modalNo = document.querySelector(".modal__action--negative");
+let toggleButton = document.querySelector(".toggle-button");
+let mobileNav = document.querySelector(".mobile-nav");
 
 let backdropAndModalShowOrHide = () => {
+  console.log("click");
   if (backdrop.style.display === "block") {
     backdrop.style.display = "none";
     modal.style.display = "none";
+    if (mobileNav.style.display === "block") {
+      mobileNav.style.display = "none";
+    }
   } else {
     backdrop.style.display = "block";
     modal.style.display = "block";
@@ -19,4 +25,10 @@ selectPlanButton.forEach((btn) => {
 
 backdrop.addEventListener("click", backdropAndModalShowOrHide);
 
-modalNo.addEventListener("click", backdropAndModalShowOrHide);
+modalNoButton.addEventListener("click", backdropAndModalShowOrHide);
+
+toggleButton.addEventListener("click", () => {
+  console.log("click");
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
