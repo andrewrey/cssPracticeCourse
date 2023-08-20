8ca601d903c6d4815c383705808e18a6dfad344f
Andrew <andrew_reynolds@mac.com>
2023-08-19 23:30:02 -0700

    added modal show and hide functionality

--- shared.js
+++ shared.js
@@ -1,3 +1,22 @@
 let backdrop = document.querySelector(".backdrop");
+let modal = document.querySelector(".modal");
+let selectPlanButton = document.querySelectorAll(".plan button");
+let modalNo = document.querySelector(".modal__action--negative");
 
-backdrop.style.display = "block";
+let backdropAndModalShowOrHide = () => {
+  if (backdrop.style.display === "block") {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+  } else {
+    backdrop.style.display = "block";
+    modal.style.display = "block";
+  }
+};
+
+selectPlanButton.forEach((btn) => {
+  btn.addEventListener("click", backdropAndModalShowOrHide);
+});
+
+backdrop.addEventListener("click", backdropAndModalShowOrHide);
+
+modalNo.addEventListener("click", backdropAndModalShowOrHide);
